93ec5d0d85de6ce2eabfe460ca19e88e5b742a85
Baptiste Wicht <baptiste.wicht@gmail.com>
2017-03-06 10:20:43 +0100

    Initial implementations of sum functions

---
+++ src/sum.cu
@@ -0,0 +1,24 @@
+//=======================================================================
+// Copyright (c) 2017 Baptiste Wicht
+// Distributed under the terms of the MIT License.
+// (See accompanying file LICENSE or copy at
+//  http://opensource.org/licenses/MIT)
+//=======================================================================
+
+#include <thrust/reduce.h>
+
+#include "cpp_utils/assert.hpp"
+
+float egblas_ssum(float* x, size_t n, size_t s){
+    cpp_assert(s == 1, "Stride is not yet supported for egblas_ssum");
+    cpp_unused(s);
+
+    return thrust::reduce(x, x + n);
+}
+
+float egblas_dsum(float* x, size_t n, size_t s){
+    cpp_assert(s == 1, "Stride is not yet supported for egblas_dsum");
+    cpp_unused(s);
+
+    return thrust::reduce(x, x + n);
+}
