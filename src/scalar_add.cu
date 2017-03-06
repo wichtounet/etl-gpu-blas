2d1f7a8e714aa9d4dda2ba602894a05f00d7507b
Baptiste Wicht <baptiste.wicht@gmail.com>
2017-03-06 13:08:16 +0100

    Support for stride in scalar_add

--- src/scalar_add.cu
+++ src/scalar_add.cu
@@ -5,44 +5,34 @@
 //  http://opensource.org/licenses/MIT)
 //=======================================================================
 
-#include "cpp_utils/assert.hpp"
-
 template<typename T>
-__global__ void scalar_add_kernel(T * x, size_t n, const T beta){
-    auto index = threadIdx.x + blockIdx.x*blockDim.x;
-    auto stride = blockDim.x * gridDim.x;
+__global__ void scalar_add_kernel(T * x, size_t n, size_t s, const T beta){
+    auto index = 1 * (threadIdx.x + blockIdx.x*blockDim.x);
+    auto stride = 1 * (blockDim.x * gridDim.x);
 
     for(; index < n; index += stride) {
-        x[index] += beta;
+        x[s * index] += beta;
     }
 }
 
 template<typename T>
 void scalar_add_kernel_run(T * x, size_t n, size_t s, T beta){
-    cpp_unused(s);
-
     int blockSize;
     int minGridSize;
 
     cudaOccupancyMaxPotentialBlockSize(&minGridSize, &blockSize, scalar_add_kernel<T>, 0, 0);
 
-    int gridSize = (n + blockSize - 1) / blockSize;
+    int gridSize = ((n / s) + blockSize - 1) / blockSize;
 
-    scalar_add_kernel<T><<<gridSize, blockSize>>>(x, n, beta);
+    scalar_add_kernel<T><<<gridSize, blockSize>>>(x, n, s, beta);
 
     cudaDeviceSynchronize();
 }
 
 void egblas_scalar_sadd(float* x, size_t n, size_t s, float beta){
-    cpp_assert(s == 1, "Stride is not yet supported for egblas_scalar_sadd");
-    cpp_unused(s);
-
     scalar_add_kernel_run(x, n, s, beta);
 }
 
 void egblas_scalar_dadd(double* x, size_t n, size_t s, double beta){
-    cpp_assert(s == 1, "Stride is not yet supported for egblas_scalar_dadd");
-    cpp_unused(s);
-
     scalar_add_kernel_run(x, n, s, beta);
 }
--- test/src/scalar_add.cpp
+++ test/src/scalar_add.cpp
@@ -43,6 +43,37 @@ TEST_CASE( "scalar_add/float/0", "[float][scalar_add]"){
     delete[] cpu_vec;
 }
 
+TEST_CASE( "scalar_add/float/1", "[float][scalar_add]"){
+    const size_t N = 124;
+
+    float* cpu_vec = new float[N];
+
+    for(size_t i = 0; i < N; ++i){
+        cpu_vec[i] = i;
+    }
+
+    float* gpu_vec;
+    cuda_check(cudaMalloc((void **)&gpu_vec, N*sizeof(float)));
+
+    cuda_check(cudaMemcpy(gpu_vec, cpu_vec, N*sizeof(float), cudaMemcpyHostToDevice));
+
+    egblas_scalar_sadd(gpu_vec, N, 2, 1.5f);
+
+    cuda_check(cudaMemcpy(cpu_vec, gpu_vec, N*sizeof(float), cudaMemcpyDeviceToHost));
+
+    for(size_t i = 0; i < N; ++i){
+        if(i % 2 == 0){
+            REQUIRE(cpu_vec[i] == Approx(i + 1.5f));
+        } else {
+            REQUIRE(cpu_vec[i] == Approx(i));
+        }
+    }
+
+    cuda_check(cudaFree(gpu_vec));
+
+    delete[] cpu_vec;
+}
+
 TEST_CASE( "scalar_add/double/0", "[double][scalar_add]"){
     const size_t N = 254;
 
@@ -63,6 +94,37 @@ TEST_CASE( "scalar_add/double/0", "[double][scalar_add]"){
 
     for(size_t i = 0; i < N; ++i){
         REQUIRE(cpu_vec[i] == Approx(i + 2.2));
+    }
+
+    cuda_check(cudaFree(gpu_vec));
+
+    delete[] cpu_vec;
+}
+
+TEST_CASE( "scalar_add/double/1", "[double][scalar_add]"){
+    const size_t N = 302;
+
+    double* cpu_vec = new double[N];
+
+    for(size_t i = 0; i < N; ++i){
+        cpu_vec[i] = i;
+    }
+
+    double* gpu_vec;
+    cuda_check(cudaMalloc((void **)&gpu_vec, N*sizeof(double)));
+
+    cuda_check(cudaMemcpy(gpu_vec, cpu_vec, N*sizeof(double), cudaMemcpyHostToDevice));
+
+    egblas_scalar_dadd(gpu_vec, N, 3, 2.2f);
+
+    cuda_check(cudaMemcpy(cpu_vec, gpu_vec, N*sizeof(double), cudaMemcpyDeviceToHost));
+
+    for(size_t i = 0; i < N; ++i){
+        if(i % 3 == 0){
+            REQUIRE(cpu_vec[i] == Approx(i + 2.2));
+        } else {
+            REQUIRE(cpu_vec[i] == Approx(i));
+        }
     }
 
     cuda_check(cudaFree(gpu_vec));
