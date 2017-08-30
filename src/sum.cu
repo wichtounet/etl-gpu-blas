9ccc4ecf62be7753b4a5a52cf4cd81bbe906dc1a
Baptiste Wicht <baptiste.wicht@gmail.com>
2017-08-30 12:56:20 +0200

    Review ssum and dsum

--- src/sum.cu
+++ src/sum.cu
@@ -10,20 +10,85 @@
 
 #include "egblas/assert.hpp"
 #include "egblas/sum.hpp"
+#include "egblas/cuda_check.hpp"
+
+template <typename T>
+__global__ void sum_kernel(size_t n, const T* g_idata, size_t incx, T* g_odata) {
+    extern __shared__ __align__(sizeof(T)) unsigned char sdata_raw[];
+
+    T* sdata = reinterpret_cast<T*>(sdata_raw);
+
+    // each thread loads one element from global to shared mem
+    auto tid = threadIdx.x;
+    auto i   = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if (i < n) {
+        sdata[tid] = g_idata[i * incx];
+    } else {
+        sdata[tid] = T(0);
+    }
+
+    __syncthreads();
+
+    // do reduction in shared mem
+    for (size_t s = 1; s < blockDim.x; s *= 2) {
+        if (tid % (2 * s) == 0) {
+            sdata[tid] += sdata[tid + s];
+        }
+
+        __syncthreads();
+    }
+
+    // write result for this block to global mem
+    if (tid == 0) {
+        g_odata[blockIdx.x] = sdata[0];
+    }
+}
+
+template <typename T>
+T sum_kernel_run(size_t n, const T* input, size_t incx) {
+    int blockSize;
+    int minGridSize;
+
+    cudaOccupancyMaxPotentialBlockSize(&minGridSize, &blockSize, sum_kernel<T>, 0, 0);
+
+    T* y_cpu = new T[blockSize];
+
+    T* y_gpu;
+    cuda_check(cudaMalloc((void**)&y_gpu, blockSize * sizeof(T)));
+
+    int gridSize = ((n / incx) + blockSize - 1) / blockSize;
+
+    sum_kernel<T><<<gridSize, blockSize, blockSize * sizeof(T)>>>(n, input, incx, y_gpu);
+
+    cudaDeviceSynchronize();
+
+    cuda_check(cudaMemcpy(y_cpu, y_gpu, blockSize * sizeof(T), cudaMemcpyDeviceToHost));
+
+    cuda_check(cudaFree(y_gpu));
+
+    T sum = y_cpu[0];
+
+    delete[] y_cpu;
+
+    return sum;
+}
 
 float egblas_ssum(float* x, size_t n, size_t s) {
     egblas_assert(s == 1, "Stride is not yet supported for egblas_ssum");
     egblas_unused(s);
 
-    return thrust::reduce(thrust::device, x, x + n);
+    return sum_kernel_run(n, x, s);
 }
 
 double egblas_dsum(double* x, size_t n, size_t s) {
     egblas_assert(s == 1, "Stride is not yet supported for egblas_dsum");
     egblas_unused(s);
 
-    return thrust::reduce(thrust::device, x, x + n);
+    return sum_kernel_run(n, x, s);
 }
+
+// Complex sums are done with thrust
 
 struct single_complex_add {
     __device__ cuComplex operator()(cuComplex x, cuComplex y) {
