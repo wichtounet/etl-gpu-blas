c57f66d87c8be4605f37b17472bd8a3c73b43c41
Baptiste Wicht <baptiste.wicht@gmail.com>
2017-08-30 08:09:39 +0200

    Add cuda_check for the library

---
+++ include/egblas/cuda_check.hpp
@@ -0,0 +1,16 @@
+//=======================================================================
+// Copyright (c) 2017 Baptiste Wicht
+// Distributed under the terms of the MIT License.
+// (See accompanying file LICENSE or copy at
+//  http://opensource.org/licenses/MIT)
+//=======================================================================
+
+#define cuda_check(call)                                                                                \
+    {                                                                                                   \
+        auto status = call;                                                                             \
+        if (status != cudaSuccess) {                                                                    \
+            std::cerr << "CUDA error: " << cudaGetErrorString(status) << " from " << #call << std::endl \
+                      << "from " << __FILE__ << ":" << __LINE__ << std::endl;                           \
+            exit(-1);                                                                                   \
+        }                                                                                               \
+    }
