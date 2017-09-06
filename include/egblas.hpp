59e406f0da75c54b3bd1c355240bc7c8b1381d4c
Baptiste Wicht <baptiste.wicht@gmail.com>
2017-09-06 08:07:11 +0200

    Cleanup

--- include/egblas.hpp
+++ include/egblas.hpp
@@ -7,14 +7,23 @@
 
 #pragma once
 
+// Binary functions
 #include "egblas/axdy.hpp"
 #include "egblas/axmy.hpp"
+
+// Scalar manipulations
 #include "egblas/scalar_set.hpp"
 #include "egblas/scalar_add.hpp"
 #include "egblas/scalar_div.hpp"
-#include "egblas/sum.hpp"
+
+// Unary element-wise functions
 #include "egblas/sqrt.hpp"
 #include "egblas/log.hpp"
 #include "egblas/exp.hpp"
+
+// Reductions
+#include "egblas/sum.hpp"
+
+// Machine Learning Special functions
 #include "egblas/cce.hpp"
 #include "egblas/relu_der_out.hpp"
