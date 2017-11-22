f3dcb7f20e211ef4df9c432bd8443e0adae1fba3
Baptiste Wicht <baptiste.wicht@gmail.com>
2017-11-22 20:20:21 +0100

    Refactor

--- src/axmy.cu
+++ src/axmy.cu
@@ -7,33 +7,15 @@
 
 #include "egblas/axmy.hpp"
 
+#include "complex.hpp"
+
 template <typename T>
 __global__ void axmy_kernel(size_t n, T alpha, const T* x, size_t incx, T* y, size_t incy) {
     auto index  = threadIdx.x + blockIdx.x * blockDim.x;
     auto stride = blockDim.x * gridDim.x;
 
     for (; index < n; index += stride) {
-        y[incy * index] = alpha * x[incx * index] * y[incy * index];
-    }
-}
-
-template <>
-__global__ void axmy_kernel(size_t n, cuComplex alpha, const cuComplex* x, size_t incx, cuComplex* y, size_t incy) {
-    auto index  = threadIdx.x + blockIdx.x * blockDim.x;
-    auto stride = blockDim.x * gridDim.x;
-
-    for (; index < n; index += stride) {
-        y[incy * index] = cuCmulf(cuCmulf(alpha, x[incx * index]), y[incy * index]);
-    }
-}
-
-template <>
-__global__ void axmy_kernel(size_t n, cuDoubleComplex alpha, const cuDoubleComplex* x, size_t incx, cuDoubleComplex* y, size_t incy) {
-    auto index  = threadIdx.x + blockIdx.x * blockDim.x;
-    auto stride = blockDim.x * gridDim.x;
-
-    for (; index < n; index += stride) {
-        y[incy * index] = cuCmul(cuCmul(alpha, x[incx * index]), y[incy * index]);
+        y[incy * index] = alpha * (x[incx * index] * y[incy * index]);
     }
 }
 
@@ -47,53 +29,13 @@ __global__ void axmy_kernel1(size_t n, const T* x, size_t incx, T* y, size_t inc
     }
 }
 
-template <>
-__global__ void axmy_kernel1(size_t n, const cuComplex* x, size_t incx, cuComplex* y, size_t incy) {
-    auto index  = threadIdx.x + blockIdx.x * blockDim.x;
-    auto stride = blockDim.x * gridDim.x;
-
-    for (; index < n; index += stride) {
-        y[incy * index] = cuCmulf(x[incx * index], y[incy * index]);
-    }
-}
-
-template <>
-__global__ void axmy_kernel1(size_t n, const cuDoubleComplex* x, size_t incx, cuDoubleComplex* y, size_t incy) {
-    auto index  = threadIdx.x + blockIdx.x * blockDim.x;
-    auto stride = blockDim.x * gridDim.x;
-
-    for (; index < n; index += stride) {
-        y[incy * index] = cuCmul(x[incx * index], y[incy * index]);
-    }
-}
-
 template <typename T>
 __global__ void axmy_kernel0(size_t n, T* y, size_t incy) {
     auto index  = threadIdx.x + blockIdx.x * blockDim.x;
     auto stride = blockDim.x * gridDim.x;
 
     for (; index < n; index += stride) {
-        y[incy * index] = T(0);
-    }
-}
-
-template <>
-__global__ void axmy_kernel0(size_t n, cuComplex* y, size_t incy) {
-    auto index  = threadIdx.x + blockIdx.x * blockDim.x;
-    auto stride = blockDim.x * gridDim.x;
-
-    for (; index < n; index += stride) {
-        y[incy * index] = make_cuComplex(0, 0);
-    }
-}
-
-template <>
-__global__ void axmy_kernel0(size_t n, cuDoubleComplex* y, size_t incy) {
-    auto index  = threadIdx.x + blockIdx.x * blockDim.x;
-    auto stride = blockDim.x * gridDim.x;
-
-    for (; index < n; index += stride) {
-        y[incy * index] = make_cuDoubleComplex(0, 0);
+        y[incy * index] = zero<T>();
     }
 }
 
