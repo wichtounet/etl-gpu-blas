8ca228390137cc05f59747e2806462afcf30ba95
Baptiste Wicht <baptiste.wicht@gmail.com>
2017-11-27 21:34:59 +0100

    Tests for normalize_sub

--- test/src/normalize.cpp
+++ test/src/normalize.cpp
@@ -7,7 +7,7 @@
 
 #include "test.hpp"
 
-TEST_CASE("normalize/0", "[normalize]") {
+TEST_CASE("normalize_flat/0", "[normalize]") {
     const size_t N = 137;
 
     float* a_cpu = new float[N];
@@ -41,6 +41,49 @@ TEST_CASE("normalize/0", "[normalize]") {
 
     REQUIRE(m == Approx(0.0f));
     REQUIRE(s == Approx(1.0f));
+
+    cuda_check(cudaFree(a_gpu));
+
+    delete[] a_cpu;
+}
+
+TEST_CASE("normalize_sub/0", "[normalize]") {
+    const size_t N1 = 3;
+    const size_t N = 137;
+
+    float* a_cpu = new float[N1 * N];
+
+    for (size_t i = 0; i < N1 * N; ++i) {
+        a_cpu[i] = i;
+    }
+
+    float* a_gpu;
+    cuda_check(cudaMalloc((void**)&a_gpu, N1 * N * sizeof(float)));
+
+    cuda_check(cudaMemcpy(a_gpu, a_cpu, N1 * N * sizeof(float), cudaMemcpyHostToDevice));
+
+    egblas_snormalize_sub(N1, a_gpu, N, 1);
+
+    cuda_check(cudaMemcpy(a_cpu, a_gpu, N1 * N * sizeof(float), cudaMemcpyDeviceToHost));
+
+    for (size_t ss = 0; ss < N1; ++ss) {
+        float m = 0;
+        for (size_t i = 0; i < N; ++i) {
+            m += a_cpu[ss * N + i];
+        }
+
+        m = m / N;
+
+        float s = 0;
+        for (size_t i = 0; i < N; ++i) {
+            s += (a_cpu[ss * N + i] - m) * (a_cpu[ss * N + i] - m);
+        }
+
+        s = std::sqrt(s / N);
+
+        REQUIRE(m == Approx(0.0f));
+        REQUIRE(s == Approx(1.0f));
+    }
 
     cuda_check(cudaFree(a_gpu));
 
