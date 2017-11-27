86402c51eec7511ac52229803c8ce3e70d565151
Baptiste Wicht <baptiste.wicht@gmail.com>
2017-11-27 20:33:59 +0100

    Standard deviation tests

---
+++ test/src/stddev.cpp
@@ -0,0 +1,165 @@
+//=======================================================================
+// Copyright (c) 2017 Baptiste Wicht
+// Distributed under the terms of the MIT License.
+// (See accompanying file LICENSE or copy at
+//  http://opensource.org/licenses/MIT)
+//=======================================================================
+
+#include <complex>
+#include <iostream>
+
+#include "cuda.h"
+#include "cuda_runtime.h"
+#include "cuda_runtime_api.h"
+
+#include "egblas.hpp"
+#include "test.hpp"
+
+#include "catch.hpp"
+
+TEST_CASE("stddev/s/0", "[float][stddev]") {
+    const size_t N = 257;
+
+    float* cpu_vec = new float[N];
+
+    for (size_t i = 0; i < N; ++i) {
+        cpu_vec[i] = i + 1;
+    }
+
+    float* gpu_vec;
+    cuda_check(cudaMalloc((void**)&gpu_vec, N * sizeof(float)));
+
+    cuda_check(cudaMemcpy(gpu_vec, cpu_vec, N * sizeof(float), cudaMemcpyHostToDevice));
+
+    REQUIRE(egblas_sstddev(gpu_vec, N, 1) == Approx(float(74.18895)));
+
+    cuda_check(cudaFree(gpu_vec));
+
+    delete[] cpu_vec;
+}
+
+TEST_CASE("stddev/s/1", "[float][stddev]") {
+    const size_t N = 389;
+
+    float* cpu_vec = new float[N];
+
+    for (size_t i = 0; i < N; ++i) {
+        cpu_vec[i] = (3.1 * i) / 2.0;
+    }
+
+    float* gpu_vec;
+    cuda_check(cudaMalloc((void**)&gpu_vec, N * sizeof(float)));
+
+    cuda_check(cudaMemcpy(gpu_vec, cpu_vec, N * sizeof(float), cudaMemcpyHostToDevice));
+
+    REQUIRE(egblas_sstddev(gpu_vec, N, 1) == Approx(float(174.05611)));
+
+    cuda_check(cudaFree(gpu_vec));
+
+    delete[] cpu_vec;
+}
+
+TEST_CASE("stddev/s/2", "[float][stddev]") {
+    const size_t N = 1024 * 128 + 5;
+
+    float* cpu_vec = new float[N];
+
+    for (size_t i = 0; i < N; ++i) {
+        cpu_vec[i] = i + 1;
+    }
+
+    float* gpu_vec;
+    cuda_check(cudaMalloc((void**)&gpu_vec, N * sizeof(float)));
+
+    cuda_check(cudaMemcpy(gpu_vec, cpu_vec, N * sizeof(float), cudaMemcpyHostToDevice));
+
+    REQUIRE(egblas_sstddev(gpu_vec, N, 1) == Approx(float(37838.67188)));
+
+    cuda_check(cudaFree(gpu_vec));
+
+    delete[] cpu_vec;
+}
+
+TEST_CASE("stddev/s/3", "[float][stddev]") {
+    const size_t N = 1024 * 1024 * 6 + 126;
+
+    float* cpu_vec = new float[N];
+
+    for (size_t i = 0; i < N; ++i) {
+        cpu_vec[i] = i + 1;
+    }
+
+    float* gpu_vec;
+    cuda_check(cudaMalloc((void**)&gpu_vec, N * sizeof(float)));
+
+    cuda_check(cudaMemcpy(gpu_vec, cpu_vec, N * sizeof(float), cudaMemcpyHostToDevice));
+
+    REQUIRE(egblas_sstddev(gpu_vec, N, 1) == Approx(float(1816223.375)));
+
+    cuda_check(cudaFree(gpu_vec));
+
+    delete[] cpu_vec;
+}
+
+TEST_CASE("stddev/d/0", "[double][stddev]") {
+    const size_t N = 123;
+
+    double* cpu_vec = new double[N];
+
+    for (size_t i = 0; i < N; ++i) {
+        cpu_vec[i] = i;
+    }
+
+    double* gpu_vec;
+    cuda_check(cudaMalloc((void**)&gpu_vec, N * sizeof(double)));
+
+    cuda_check(cudaMemcpy(gpu_vec, cpu_vec, N * sizeof(double), cudaMemcpyHostToDevice));
+
+    REQUIRE(egblas_dstddev(gpu_vec, N, 1) == Approx(double(35.50586)));
+
+    cuda_check(cudaFree(gpu_vec));
+
+    delete[] cpu_vec;
+}
+
+TEST_CASE("stddev/d/1", "[double][stddev]") {
+    const size_t N = 389;
+
+    double* cpu_vec = new double[N];
+
+    for (size_t i = 0; i < N; ++i) {
+        cpu_vec[i] = (3.1 * i) / 2.0;
+    }
+
+    double* gpu_vec;
+    cuda_check(cudaMalloc((void**)&gpu_vec, N * sizeof(double)));
+
+    cuda_check(cudaMemcpy(gpu_vec, cpu_vec, N * sizeof(double), cudaMemcpyHostToDevice));
+
+    REQUIRE(egblas_dstddev(gpu_vec, N, 1) == Approx(double(174.056)));
+
+    cuda_check(cudaFree(gpu_vec));
+
+    delete[] cpu_vec;
+}
+
+TEST_CASE("stddev/d/2", "[double][stddev]") {
+    const size_t N = 5;
+
+    double* cpu_vec = new double[N];
+
+    for (size_t i = 0; i < N; ++i) {
+        cpu_vec[i] = i;
+    }
+
+    double* gpu_vec;
+    cuda_check(cudaMalloc((void**)&gpu_vec, N * sizeof(double)));
+
+    cuda_check(cudaMemcpy(gpu_vec, cpu_vec, N * sizeof(double), cudaMemcpyHostToDevice));
+
+    REQUIRE(egblas_dstddev(gpu_vec, N, 1) == Approx(double(1.41421)));
+
+    cuda_check(cudaFree(gpu_vec));
+
+    delete[] cpu_vec;
+}
