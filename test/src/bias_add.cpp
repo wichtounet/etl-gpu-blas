282fa6880868fed647955cb2e07e0c59b6fcb6f2
Baptiste Wicht <baptiste.wicht@gmail.com>
2017-11-28 20:14:13 +0100

    Complete the tests

--- test/src/bias_add.cpp
+++ test/src/bias_add.cpp
@@ -88,3 +88,89 @@ TEST_CASE("bias_add_2d/d/0", "[double][abs]") {
     delete[] x_cpu;
     delete[] y_cpu;
 }
+
+TEST_CASE("bias_add_4d/s/0", "[float][abs]") {
+    const size_t M = 13;
+    const size_t N = 9;
+    const size_t O = 11;
+    const size_t P = 8;
+
+    double* x_cpu = new double[M * N * O * P];
+    double* y_cpu = new double[M * N * O * P];
+    double* b_cpu = new double[N];
+
+    for (size_t i = 0; i < M * N; ++i) {
+        x_cpu[i] = 1.09 * i;
+    }
+
+    for (size_t i = 0; i < N; ++i) {
+        b_cpu[i] = 2.04f * (i + 1);
+    }
+
+    double* x_gpu;
+    double* y_gpu;
+    double* b_gpu;
+    cuda_check(cudaMalloc((void**)&x_gpu, M * N * O * P * sizeof(double)));
+    cuda_check(cudaMalloc((void**)&y_gpu, M * N * O * P * sizeof(double)));
+    cuda_check(cudaMalloc((void**)&b_gpu, N * sizeof(double)));
+
+    cuda_check(cudaMemcpy(x_gpu, x_cpu, M * N * O * P * sizeof(double), cudaMemcpyHostToDevice));
+    cuda_check(cudaMemcpy(b_gpu, b_cpu, N * sizeof(double), cudaMemcpyHostToDevice));
+
+    egblas_dbias_add_4d(M, N, O, P, x_gpu, 1, b_gpu, 1, y_gpu, 1);
+
+    cuda_check(cudaMemcpy(y_cpu, y_gpu, M * N * O * P * sizeof(double), cudaMemcpyDeviceToHost));
+
+    for (size_t i = 0; i < M * N * O * P; ++i) {
+        REQUIRE(y_cpu[i] == Approx(x_cpu[i] + b_cpu[(i / (O * P)) % N]));
+    }
+
+    cuda_check(cudaFree(x_gpu));
+    cuda_check(cudaFree(y_gpu));
+
+    delete[] x_cpu;
+    delete[] y_cpu;
+}
+
+TEST_CASE("bias_add_4d/d/0", "[float][abs]") {
+    const size_t M = 3;
+    const size_t N = 9;
+    const size_t O = 7;
+    const size_t P = 5;
+
+    double* x_cpu = new double[M * N * O * P];
+    double* y_cpu = new double[M * N * O * P];
+    double* b_cpu = new double[N];
+
+    for (size_t i = 0; i < M * N; ++i) {
+        x_cpu[i] = i;
+    }
+
+    for (size_t i = 0; i < N; ++i) {
+        b_cpu[i] = i + 1;
+    }
+
+    double* x_gpu;
+    double* y_gpu;
+    double* b_gpu;
+    cuda_check(cudaMalloc((void**)&x_gpu, M * N * O * P * sizeof(double)));
+    cuda_check(cudaMalloc((void**)&y_gpu, M * N * O * P * sizeof(double)));
+    cuda_check(cudaMalloc((void**)&b_gpu, N * sizeof(double)));
+
+    cuda_check(cudaMemcpy(x_gpu, x_cpu, M * N * O * P * sizeof(double), cudaMemcpyHostToDevice));
+    cuda_check(cudaMemcpy(b_gpu, b_cpu, N * sizeof(double), cudaMemcpyHostToDevice));
+
+    egblas_dbias_add_4d(M, N, O, P, x_gpu, 1, b_gpu, 1, y_gpu, 1);
+
+    cuda_check(cudaMemcpy(y_cpu, y_gpu, M * N * O * P * sizeof(double), cudaMemcpyDeviceToHost));
+
+    for (size_t i = 0; i < M * N * O * P; ++i) {
+        REQUIRE(y_cpu[i] == Approx(x_cpu[i] + b_cpu[(i / (O * P)) % N]));
+    }
+
+    cuda_check(cudaFree(x_gpu));
+    cuda_check(cudaFree(y_gpu));
+
+    delete[] x_cpu;
+    delete[] y_cpu;
+}
