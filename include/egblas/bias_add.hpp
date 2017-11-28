db482d586183b7933dd01536b2b39c35a589a0b0
Baptiste Wicht <baptiste.wicht@gmail.com>
2017-11-28 20:14:09 +0100

    Support for bias_add_4d

--- include/egblas/bias_add.hpp
+++ include/egblas/bias_add.hpp
@@ -35,5 +35,37 @@ void egblas_sbias_add_2d(size_t m, size_t n, const float* x, size_t incx, const 
  */
 void egblas_dbias_add_2d(size_t m, size_t n, const double* x, size_t incx, const double* b, size_t incb, double* y, size_t incy);
 
+/*!
+ * \brief Add the 1D bias to to the batched 1D input
+ * \param m The first dimension of x and y
+ * \param n The second dimension of x and y
+ * \param o The third dimension of x and y
+ * \param p The fourth dimension of x and y
+ * \param x The vector x (GPU memory)
+ * \param incx The stride of x
+ * \param b The vector x (GPU memory)
+ * \param incb The stride of x
+ * \param y The vector y (GPU memory)
+ * \param incy The stride of y
+ */
+void egblas_sbias_add_4d(size_t m, size_t n, size_t o, size_t p, const float* x, size_t incx, const float* b, size_t incb, float* y, size_t incy);
+
+/*!
+ * \brief Add the 1D bias to to the batched 1D input
+ * \param m The first dimension of x and y
+ * \param n The second dimension of x and y
+ * \param o The third dimension of x and y
+ * \param p The fourth dimension of x and y
+ * \param x The vector x (GPU memory)
+ * \param incx The stride of x
+ * \param b The vector x (GPU memory)
+ * \param incb The stride of x
+ * \param y The vector y (GPU memory)
+ * \param incy The stride of y
+ */
+void egblas_dbias_add_4d(size_t m, size_t n, size_t o, size_t p, const double* x, size_t incx, const double* b, size_t incb, double* y, size_t incy);
+
 #define EGBLAS_HAS_SBIAS_ADD_2D true
 #define EGBLAS_HAS_DBIAS_ADD_2D true
+#define EGBLAS_HAS_SBIAS_ADD_4D true
+#define EGBLAS_HAS_DBIAS_ADD_4D true
--- src/bias_add.cu
+++ src/bias_add.cu
@@ -18,6 +18,16 @@ __global__ void bias_add_2d_kernel(size_t m, size_t n, const T* x, size_t incx, 
 }
 
 template <typename T>
+__global__ void bias_add_4d_kernel(size_t m, size_t n, size_t o, size_t p, const T* x, size_t incx, const T* b, size_t incb, T* y, size_t incy) {
+    auto index  = threadIdx.x + blockIdx.x * blockDim.x;
+    const auto stride = blockDim.x * gridDim.x;
+
+    for (; index < m * n * o * p; index += stride) {
+        y[incy * index] = x[incx * index] + b[((index / (o * p)) % n) * incb];
+    }
+}
+
+template <typename T>
 void bias_add_2d_kernel_run(size_t m, size_t n, const T* x, size_t incx, const T* b, size_t incb, T* y, size_t incy) {
     int blockSize;
     int minGridSize;
@@ -33,10 +43,34 @@ void bias_add_2d_kernel_run(size_t m, size_t n, const T* x, size_t incx, const T
 #endif
 }
 
+template <typename T>
+void bias_add_4d_kernel_run(size_t m, size_t n, size_t o, size_t p, const T* x, size_t incx, const T* b, size_t incb, T* y, size_t incy) {
+    int blockSize;
+    int minGridSize;
+
+    cudaOccupancyMaxPotentialBlockSize(&minGridSize, &blockSize, bias_add_4d_kernel<T>, 0, 0);
+
+    int gridSize = (((m * n * o * p) / incy) + blockSize - 1) / blockSize;
+
+    bias_add_4d_kernel<T><<<gridSize, blockSize>>>(m, n, o, p, x, incx, b, incb, y, incy);
+
+#ifdef EGBLAS_SYNCHRONIZE
+    cudaDeviceSynchronize();
+#endif
+}
+
 void egblas_sbias_add_2d(size_t m, size_t n, const float* x, size_t incx, const float* b, size_t incb, float* y, size_t incy){
     bias_add_2d_kernel_run(m, n, x, incx, b, incb, y, incy);
 }
 
 void egblas_dbias_add_2d(size_t m, size_t n, const double* x, size_t incx, const double* b, size_t incb, double* y, size_t incy){
     bias_add_2d_kernel_run(m, n, x, incx, b, incb, y, incy);
+}
+
+void egblas_sbias_add_4d(size_t m, size_t n, size_t o, size_t p, const float* x, size_t incx, const float* b, size_t incb, float* y, size_t incy){
+    bias_add_4d_kernel_run(m, n, o, p, x, incx, b, incb, y, incy);
+}
+
+void egblas_dbias_add_4d(size_t m, size_t n, size_t o, size_t p, const double* x, size_t incx, const double* b, size_t incb, double* y, size_t incy){
+    bias_add_4d_kernel_run(m, n, o, p, x, incx, b, incb, y, incy);
 }
