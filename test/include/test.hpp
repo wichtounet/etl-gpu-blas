4b2604692acfd4ac20d419f921257e0c68108dcc
Baptiste Wicht <baptiste.wicht@gmail.com>
2018-03-20 08:12:54 +0100

    New test utility: dual_array

--- test/include/test.hpp
+++ test/include/test.hpp
@@ -7,6 +7,7 @@
 
 #include <complex>
 #include <iostream>
+#include <type_traits>
 
 #include "cuda.h"
 #include "cuda_runtime.h"
@@ -28,3 +29,51 @@
     }
 
 extern float large_eps;
+
+/*!
+ * \brief Utility for testing on dual (CPU/GPU) arrays
+ */
+template<typename T>
+struct dual_array {
+    T* _cpu;
+    T* _gpu;
+    const size_t N;
+
+    explicit dual_array(size_t N) : N(N) {
+        _cpu = new T[N];
+
+        cuda_check(cudaMalloc((void**)&_gpu, N * sizeof(T)));
+    }
+
+    ~dual_array(){
+        cuda_check(cudaFree(_gpu));
+
+        delete[] _cpu;
+    }
+
+    void cpu_to_gpu(){
+        cuda_check(cudaMemcpy(_gpu, _cpu, N * sizeof(T), cudaMemcpyHostToDevice));
+    }
+
+    void gpu_to_cpu(){
+        cuda_check(cudaMemcpy(_cpu, _gpu, N * sizeof(T), cudaMemcpyDeviceToHost));
+    }
+
+    T* cpu(){
+        return _cpu;
+    }
+
+    T* gpu(){
+        return _gpu;
+    }
+
+    template<typename TT = T, typename std::enable_if<std::is_same<TT, std::complex<float>>::value, int>::type = 42>
+    cuComplex* complex_gpu(){
+        return reinterpret_cast<cuComplex*>(_gpu);
+    }
+
+    template<typename TT = T, typename std::enable_if<std::is_same<TT, std::complex<double>>::value, int>::type = 42>
+    cuDoubleComplex* complex_gpu(){
+        return reinterpret_cast<cuDoubleComplex*>(_gpu);
+    }
+};
