ba89cff3fa2ac503130a3564f4d3d0bbc06b885a
Baptiste Wicht <baptiste.wicht@gmail.com>
2020-06-26 21:59:49 +0200

    Three-Phase reduction for speed

--- src/bias_batch_sum.cu
+++ src/bias_batch_sum.cu
@@ -125,22 +125,45 @@ void egblas_dbias_batch_mean(size_t b, size_t n, double* x, size_t incx, double*
 
 // 4D Versions
 
+template <typename T>
+__global__ void bias_batch_sum4_kernel_zero(size_t B, size_t N, size_t W, size_t H, const T* x, T* y) {
+    auto base_n  = threadIdx.x + blockIdx.x * blockDim.x;
+
+    if (base_n < B * N * W) {
+        T sum = 0;
+
+        for (size_t o = 0; o < H; ++o) {
+            sum += x[base_n * H + o];
+        }
+
+        y[base_n] = sum;
+    }
+}
+
+template <typename T>
+__global__ void bias_batch_sum4_kernel_first(size_t B, size_t N, size_t W, size_t H, const T* x, T* y) {
+    auto base_n  = threadIdx.x + blockIdx.x * blockDim.x;
+
+    if (base_n < B * N) {
+        T sum = 0;
+
+        for (size_t o = 0; o < W; ++o) {
+            sum += x[base_n * W + o];
+        }
+
+        y[base_n] = sum;
+    }
+}
+
 template <bool Mean, typename T>
-__global__ void bias_batch_sum4_kernel_full(size_t B, size_t N, size_t W, size_t H, const T* x, T* y) {
+__global__ void bias_batch_sum4_kernel_second(size_t B, size_t N, size_t W, size_t H, const T* x, T* y) {
     auto n  = threadIdx.x + blockIdx.x * blockDim.x;
 
     if (n < N) {
-        const size_t IN1 = N * W * H;
-        const size_t IN2 = W * H;
-
         T sum = 0;
 
         for (size_t b = 0; b < B; ++b) {
-            const T* x_b = x + b * IN1 + n * IN2;
-
-            for (size_t o = 0; o < IN2; ++o) {
-                sum += x_b[o];
-            }
+            sum += x[b * N + n];
         }
 
         if (Mean) {
@@ -151,30 +174,50 @@ __global__ void bias_batch_sum4_kernel_full(size_t B, size_t N, size_t W, size_t
     }
 }
 
-void egblas_sbias_batch_sum4(size_t b, size_t n, size_t w, size_t h, float* x, float* y) {
-    const int blockSize = 128;
-    const int gridSize = (n + blockSize - 1) / blockSize;
+template <bool Mean, typename T>
+void egblas_sbias_batch_sum4_run(size_t b, size_t n, size_t w, size_t h, T* x, T* y) {
+    T* tmp_zero;
+    cuda_check(cudaMalloc((void**)&tmp_zero, b * n * w * sizeof(T)));
+    T* tmp_first;
+    cuda_check(cudaMalloc((void**)&tmp_first, b * n * sizeof(T)));
 
-    bias_batch_sum4_kernel_full<false><<<gridSize, blockSize>>>(b, n, w, h, x, y);
+    // Phase 0
+
+    int blockSize = 64;
+    int gridSize = (b * n * w + blockSize - 1) / blockSize;
+
+    bias_batch_sum4_kernel_zero<<<gridSize, blockSize>>>(b, n, w, h, x, tmp_zero);
+
+    // Phase 1
+
+    blockSize = 64;
+    gridSize = (b * n + blockSize - 1) / blockSize;
+
+    bias_batch_sum4_kernel_first<<<gridSize, blockSize>>>(b, n, w, h, tmp_zero, tmp_first);
+
+    // Phase 2
+
+    blockSize = 64;
+    gridSize = (n + blockSize - 1) / blockSize;
+
+    bias_batch_sum4_kernel_second<Mean><<<gridSize, blockSize>>>(b, n, w, h, tmp_first, y);
+
+    cuda_check(cudaFree(tmp_zero));
+    cuda_check(cudaFree(tmp_first));
+}
+
+void egblas_sbias_batch_sum4(size_t b, size_t n, size_t w, size_t h, float* x, float* y) {
+    egblas_sbias_batch_sum4_run<false>(b, n, w, h, x, y);
 }
 
 void egblas_dbias_batch_sum4(size_t b, size_t n, size_t w, size_t h, double* x, double* y) {
-    const int blockSize = 128;
-    const int gridSize = (n + blockSize - 1) / blockSize;
-
-    bias_batch_sum4_kernel_full<false><<<gridSize, blockSize>>>(b, n, w, h, x, y);
+    egblas_sbias_batch_sum4_run<false>(b, n, w, h, x, y);
 }
 
 void egblas_sbias_batch_mean4(size_t b, size_t n, size_t w, size_t h, float* x, float* y) {
-    const int blockSize = 128;
-    const int gridSize = (n + blockSize - 1) / blockSize;
-
-    bias_batch_sum4_kernel_full<true><<<gridSize, blockSize>>>(b, n, w, h, x, y);
+    egblas_sbias_batch_sum4_run<true>(b, n, w, h, x, y);
 }
 
 void egblas_dbias_batch_mean4(size_t b, size_t n, size_t w, size_t h, double* x, double* y) {
-    const int blockSize = 128;
-    const int gridSize = (n + blockSize - 1) / blockSize;
-
-    bias_batch_sum4_kernel_full<true><<<gridSize, blockSize>>>(b, n, w, h, x, y);
+    egblas_sbias_batch_sum4_run<true>(b, n, w, h, x, y);
 }
