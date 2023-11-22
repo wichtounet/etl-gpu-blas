e8fc2372b976d1fcdc20866faaf8c0533a57dcca
Baptiste Wicht <baptiste.wicht@gmail.com>
2023-11-22 07:03:25 +0100

    axdy_3: Fix kernel0

--- src/axdy_3.cu
+++ src/axdy_3.cu
@@ -46,24 +46,6 @@ __global__ void axdy_3_kernel1_flat(size_t n, const T* x, const T* y, T* yy) {
 }
 
 template <typename T>
-__global__ void axdy_3_kernel0(size_t n, T* yy, size_t incyy) {
-    auto index  = threadIdx.x + blockIdx.x * blockDim.x;
-
-    if (index < n) {
-        yy[incyy * index] = zero<T>();
-    }
-}
-
-template <typename T>
-__global__ void axdy_3_kernel0_flat(size_t n, T* yy) {
-    auto index  = threadIdx.x + blockIdx.x * blockDim.x;
-
-    if (index < n) {
-        yy[index] = zero<T>();
-    }
-}
-
-template <typename T>
 void axdy_3_kernel_run(size_t n, T alpha, const T* x, size_t incx, const T* y, size_t incy, T* yy, size_t incyy) {
     static int blockSize   = 0;
     static int minGridSize = 0;
@@ -107,33 +89,9 @@ void axdy_3_kernel1_run(size_t n, const T* x, size_t incx, const T* y, size_t in
 #endif
 }
 
-template <typename T>
-void axdy_3_kernel0_run(size_t n, T* yy, size_t incyy) {
-    static int blockSize   = 0;
-    static int minGridSize = 0;
-
-    if (!blockSize) {
-        cudaOccupancyMaxPotentialBlockSize(&minGridSize, &blockSize, axdy_3_kernel0<T>, 0, 0);
-    }
-
-    int gridSize = ((n / incyy) + blockSize - 1) / blockSize;
-
-    if (incyy == 1) {
-        axdy_3_kernel0_flat<T><<<gridSize, blockSize>>>(n, yy);
-    } else {
-        axdy_3_kernel0<T><<<gridSize, blockSize>>>(n, yy, incyy);
-    }
-
-#ifdef EGBLAS_SYNCHRONIZE
-    cudaDeviceSynchronize();
-#endif
-}
-
 void egblas_saxdy_3(size_t n, float alpha, const float* x, size_t incx, const float* y, size_t incy, float* yy, size_t incyy) {
     if (alpha == 1.0f) {
         axdy_3_kernel1_run(n, x, incx, y, incy, yy, incyy);
-    } else if (alpha == 0.0f) {
-        axdy_3_kernel0_run(n, yy, incyy);
     } else {
         axdy_3_kernel_run(n, alpha, x, incx, y, incy, yy, incyy);
     }
@@ -142,8 +100,6 @@ void egblas_saxdy_3(size_t n, float alpha, const float* x, size_t incx, const fl
 void egblas_daxdy_3(size_t n, double alpha, const double* x, size_t incx, const double* y, size_t incy, double* yy, size_t incyy) {
     if (alpha == 1.0) {
         axdy_3_kernel1_run(n, x, incx, y, incy, yy, incyy);
-    } else if (alpha == 0.0) {
-        axdy_3_kernel0_run(n, yy, incyy);
     } else {
         axdy_3_kernel_run(n, alpha, x, incx, y, incy, yy, incyy);
     }
@@ -152,8 +108,6 @@ void egblas_daxdy_3(size_t n, double alpha, const double* x, size_t incx, const 
 void egblas_caxdy_3(size_t n, cuComplex alpha, const cuComplex* x, size_t incx, const cuComplex* y, size_t incy, cuComplex* yy, size_t incyy) {
     if (alpha.x == 1.0f && alpha.y == 0.0f) {
         axdy_3_kernel1_run(n, x, incx, y, incy, yy, incyy);
-    } else if (alpha.x == 0.0f && alpha.y == 0.0f) {
-        axdy_3_kernel0_run(n, yy, incyy);
     } else {
         axdy_3_kernel_run(n, alpha, x, incx, y, incy, yy, incyy);
     }
@@ -162,8 +116,6 @@ void egblas_caxdy_3(size_t n, cuComplex alpha, const cuComplex* x, size_t incx, 
 void egblas_zaxdy_3(size_t n, cuDoubleComplex alpha, const cuDoubleComplex* x, size_t incx, const cuDoubleComplex* y, size_t incy, cuDoubleComplex* yy, size_t incyy) {
     if (alpha.x == 1.0 && alpha.y == 0.0) {
         axdy_3_kernel1_run(n, x, incx, y, incy, yy, incyy);
-    } else if (alpha.x == 0.0 && alpha.y == 0.0) {
-        axdy_3_kernel0_run(n, yy, incyy);
     } else {
         axdy_3_kernel_run(n, alpha, x, incx, y, incy, yy, incyy);
     }
@@ -172,8 +124,6 @@ void egblas_zaxdy_3(size_t n, cuDoubleComplex alpha, const cuDoubleComplex* x, s
 void egblas_iaxdy_3(size_t n, int32_t alpha, const int32_t* x, size_t incx, const int32_t* y, size_t incy, int32_t* yy, size_t incyy) {
     if (alpha == 1) {
         axdy_3_kernel1_run(n, x, incx, y, incy, yy, incyy);
-    } else if (alpha == 0) {
-        axdy_3_kernel0_run(n, yy, incyy);
     } else {
         axdy_3_kernel_run(n, alpha, x, incx, y, incy, yy, incyy);
     }
@@ -182,8 +132,6 @@ void egblas_iaxdy_3(size_t n, int32_t alpha, const int32_t* x, size_t incx, cons
 void egblas_laxdy_3(size_t n, int64_t alpha, const int64_t* x, size_t incx, const int64_t* y, size_t incy, int64_t* yy, size_t incyy) {
     if (alpha == 1) {
         axdy_3_kernel1_run(n, x, incx, y, incy, yy, incyy);
-    } else if (alpha == 0) {
-        axdy_3_kernel0_run(n, yy, incyy);
     } else {
         axdy_3_kernel_run(n, alpha, x, incx, y, incy, yy, incyy);
     }
